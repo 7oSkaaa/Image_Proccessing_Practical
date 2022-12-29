7495a3a8465eb72336254d9544cb8ab4c3add202
7oSkaaa <ahmed.7oskaa@gmail.com>
2022-12-29 19:07:18 +0200

    refactor: adding UI

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from tkinter import Toplevel, Button, Label, Frame, Menu, Tk
+from tkinter import Toplevel, Button, Label, Frame, Menu, Tk, PhotoImage
 from tkinter.filedialog import askopenfilename
 from PIL import Image, ImageTk
 from tkinter import ttk
@@ -10,6 +10,7 @@ from median_filtering import median_filter
 import cv2
 import json
 
+
 # Global variables
 root = Tk()
 path = ""
@@ -17,15 +18,17 @@ img_label = Label()
 la = Label()
 img_name = ""
 data = json.load(open("Data.json"))
+filter_seperator = ttk.Separator(root, orient="horizontal", style="TSeparator")
+output_label = Label()
 
 
 def print_description(info):
-    filewin = Toplevel(root)
+    filewin = Toplevel(root, background='#45438a')
     filewin.title("Description")
     filewin.geometry("600x400")
-    Title = Label(filewin, text=info["title"], wraplength=300, justify="center", font="Arial 16 bold")
-    Title.pack()
-    Description = Label(filewin, text=info["description"], wraplength=300, font="Arial 12", justify="left")
+    Title = Label(filewin, text=info["title"], wraplength=500, justify="center", font="Arial 16 bold", width=500, background='#45438a', foreground="white")
+    Title.pack(pady=(0, 20))
+    Description = Label(filewin, text=info["description"], wraplength=500, font="Arial 12", justify="left", width=500, background='#45438a', foreground="white")
     Description.pack(pady=(10, 0))
 
 
@@ -35,22 +38,28 @@ def get_image_name():
 
 
 def show_filter(func):
+    global filter_seperator, output_label
+    filter_seperator.destroy()
+    output_label.destroy()
+    filter_seperator = ttk.Separator(root, orient="horizontal", style="TSeparator")
+    filter_seperator.pack(fill="x", pady=(30, 0))
     global img_label
     img_label.destroy()
     img = cv2.imread(path, cv2.IMREAD_COLOR)
     img, output_path = func(img=img, img_name=img_name)
     img = Image.open(output_path)
-    image = img.resize((200, 200))
+    image = img.resize((280, 280))
+    output_label = Label(root, text="Output image", font='Arial 12 bold', foreground='#45438a')
+    output_label.pack(pady=(10, 0))
     my_img = ImageTk.PhotoImage(image)
     img_label = Label(
-        root, image=my_img, width=200, height=200, borderwidth=1, relief="solid"
-    )
+        root, image=my_img, width=280, height=280, borderwidth=1, relief="solid")
     img_label.image = my_img
-    img_label.pack(pady=20)
+    img_label.pack(pady=(20, 0))
 
 
 def load_filters():
-    label = Label(root, text="Choose any filter to start")
+    label = Label(root, text="Choose any filter to start", font='Arial 12 bold', foreground='#45438a')
     label.pack(pady=5)
     frame = Frame(root)
     filters = [
@@ -61,7 +70,7 @@ def load_filters():
     ]
     functions = [average_filter, contrast_stretching, histo_eq, median_filter]
     for x in range(len(filters)):
-        btn = Button(frame, text=filters[x], command=partial(show_filter, functions[x]))
+        btn = Button(frame, text=filters[x], command=partial(show_filter, functions[x]), font='Arial 12 bold', borderwidth=0, width=20, height=2, background='#45438a', fg='white')
         btn.grid(row=0, column=x, padx=2)
     frame.pack()
 
@@ -79,31 +88,31 @@ def pick_image():
     if len(path) > 0:
         try:
             image = Image.open(path)
-            image = image.resize((200, 200))
+            image = image.resize((280, 280))
             my_img = ImageTk.PhotoImage(image)
             global la
             la.destroy()
-            label = Label(root, text="Chosen image")
+            label = Label(root, text="Chosen image", font='Arial 12 bold', foreground='#45438a')
             label.pack(pady=(5, 0))
             la = Label(
-                root, image=my_img, width=200, height=200, borderwidth=1, relief="solid"
-            )
+                root, image=my_img, width=280, height=280, borderwidth=1, relief="solid")
             la.image = my_img
             la.pack(pady=(5, 10))
             separator = ttk.Separator(root, orient="horizontal")
-            separator.pack(fill="x")
+            separator.pack(fill="x", pady=20)
             get_image_name()
             load_filters()
-        except Exception:
-            la = Label(root, text="Please choose a valid image!")
+        except Exception as e:
+            print(e)
+            la = Label(root, text="Please choose a valid image!", font='Arial 12 bold', fg='red')
             la.pack(pady=10)
     else:
-        la = Label(root, text="Click File and choose open to open an image")
+        la = Label(root, text="Click File and choose open to open an image", font='Arial 12 bold', fg='45438a')
         la.pack(pady=10)
 
 
 def create_menu():
-    menubar = Menu(root)
+    menubar = Menu(root, tearoff=0, background='#45438a', foreground='white', font='Arial 12 bold')
     filemenu = Menu(menubar, tearoff=0)
     filemenu.add_command(label="Open", command=pick_image)
     filemenu.add_separator()
@@ -118,19 +127,25 @@ def create_menu():
 
     root.config(menu=menubar)
 
+    global filter_seperator
+    filter_seperator = ttk.Separator(root, orient="horizontal", style="TSeparator")
+
 
 def main():
+    global root
     root.title("Image Processing Project")
-    root.geometry("900x600")
+    root.geometry("1080x850")
+    bg = PhotoImage(file="Images/Welcome.png")
+    label = Label(root, image=bg)
+    label.image = bg
+    label.place(x=0, y=0, height=850, width=1080)
+    label.pack()
     ###############
     #             #
     # Design here #
     #             #
     ###############
     create_menu()
-    global la
-    la = Label(root, text="Click File and choose open to open an image")
-    la.pack(pady=10)
 
     # Start rendering the application
     root.mainloop()
--- median_filtering.py
+++ median_filtering.py
@@ -14,21 +14,21 @@ def median_filter(img, img_name):
     img_new1 = np.zeros([m, n, rgb])
 
     sz = 4
-    for i in range(1, m - 1):
-        for j in range(1, n - 1):
+    for x in range(1, m - 1):
+        for y in range(1, n - 1):
             for color in range(rgb):
                 tmp = []
-                for ii in range(-sz, sz):
-                    for jj in range(-sz, sz):
-                        xx = i + ii
-                        yy = j + jj
-                        if xx < 0 or xx >= m:
+                for dx in range(-sz, sz):
+                    for dy in range(-sz, sz):
+                        new_x = x + dx
+                        new_y = y + dy
+                        if new_x < 0 or new_x >= m:
                             continue
-                        if yy < 0 or yy >= n:
+                        if new_y < 0 or new_y >= n:
                             continue
-                        tmp.append(img[xx, yy, color])
+                        tmp.append(img[new_x, new_y, color])
                 tmp.sort()
-                img_new1[i, j, color] = tmp[int(len(tmp) / 2)]
+                img_new1[x, y, color] = tmp[int(len(tmp) / 2)]
 
     img_new1 = img_new1.astype(np.uint8)
     output_path = f'EditedImages/median_filtering_{img_name}'
