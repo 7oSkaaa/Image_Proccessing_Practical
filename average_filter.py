405dd66048c6256e013e95946ef7bf50288d25b5
7oSkaaa <ahmed.7oskaa@gmail.com>
2022-12-29 16:37:03 +0200

    refactor: adding new images

--- average_filter.py
+++ average_filter.py
@@ -8,31 +8,27 @@ def average_filter(img, img_name):
     '''Average filter'''
     # Obtain number of rows and columns
     # of the image
-    m, n = img.shape
-
-    # Develop Averaging filter(3, 3) mask
-    mask = np.full((3, 3), 2)
-    mask = mask / 9
+    m, n, rgb = img.shape
 
     # Convolve the 3X3 mask over the image
-    img_new = np.zeros([m, n])
+    img_new = np.zeros([m, n, rgb])
 
     for i in range(1, m - 1):
         for j in range(1, n - 1):
-            tmp = 0
-            for ii in range(-1, 2):
-                for jj in range(-1, 2):
-                    xx = i + ii
-                    yy = j + jj
-                    if xx < 0 or xx >= m:
-                        continue
-                    if yy < 0 or yy >= n:
-                        continue
-                    tmp += img[xx, yy]
-                    # print(ii + 1, jj + 1, mask[ii + 1, jj + 1], sep="  ")
+            for color in range(rgb):
+                tmp = 0
+                for ii in range(-1, 2):
+                    for jj in range(-1, 2):
+                        xx = i + ii
+                        yy = j + jj
+                        if xx < 0 or xx >= m:
+                            continue
+                        if yy < 0 or yy >= n:
+                            continue
+                        tmp += img[xx, yy, color]
 
-            img_new[i, j] = tmp / 9
-            # print(img_new[i, j], img[i, j], sep="   ")
+                img_new[i, j, color] = tmp / 9
 
-    cv2.imwrite(f'EditedImages/mostly_low_pass_{img_name}.jpg', img_new)
-    return img_new
+    output_path = f'EditedImages/mostly_low_pass_{img_name}'
+    cv2.imwrite(output_path, img_new)
+    return img_new, output_path
