b28c835c710f19759deaeeb9b49e7702fa5c823c
aboelsoud <mahmoudabooelsooud@gmail.com>
2022-12-29 17:40:20 +0200

    update average_filter function

--- average_filter.py
+++ average_filter.py
@@ -1,4 +1,4 @@
-# Low Pass SPatial Domain Filtering  to observe the blurring effect
+# Low Pass Spatial Domain Filtering  to observe the blurring effect
 import cv2
 import numpy as np
 
@@ -6,25 +6,23 @@ import numpy as np
 def average_filter(img, img_name):
     """Average filter"""
     '''Average filter'''
-    # Obtain number of rows and columns
-    # of the image
-    m, n, rgb = img.shape
+
+    # height, width, number of channels (RGB) in image
+    height, width, channels = img.shape
 
     # Convolve the 3X3 mask over the image
-    img_new = np.zeros([m, n, rgb])
+    img_new = np.zeros([height, width, channels])
 
-    for i in range(1, m - 1):
-        for j in range(1, n - 1):
-            for color in range(rgb):
+    for i in range(1, height - 1):
+        for j in range(1, width - 1): 
+            for color in range(channels): 
                 tmp = 0
+
                 for ii in range(-1, 2):
                     for jj in range(-1, 2):
                         xx = i + ii
                         yy = j + jj
-                        if xx < 0 or xx >= m:
-                            continue
-                        if yy < 0 or yy >= n:
-                            continue
+                        
                         tmp += img[xx, yy, color]
 
                 img_new[i, j, color] = tmp / 9
